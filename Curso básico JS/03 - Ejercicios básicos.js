05f31467cf15177d7e3ef95fa98f345161b99930
Arielss <arielm.sereno.a@gmail.com>
2025-07-17 18:39:17 -0400

    Ejercicios básicos V

---
+++ Curso básico JS/03 - Ejercicios básicos.js
@@ -0,0 +1,67 @@
+// 1. Escribe un comentario en una línea
+
+let comentarioUno = "Esto es un comentario de una sola línea para el ejercicio 1";
+
+// 2. Escribe un comentario en varias líneas
+
+let comentarioDos = "Esto es un comentario de varías líneas para el ejercicio dos. \nSe aplica salto tal cual como en Python.\nEntiendes? jaja";
+
+// 3. Declara variables con valores asociados a todos los datos primitivos
+
+let edad = 28; //int
+let esReal = true; //Bool
+let nulo = null; //Null - Object
+let indefinido = undefined; //Undefined
+let simbolo = Symbol("Simbolo para Js"); //Symbol
+let numeroUno = 465456464864n; // BigInt
+
+// 4. Imprime por consola el valor de todas las variables
+
+console.log(comentarioUno);
+console.log(comentarioDos);
+console.log(edad);
+console.log(esReal);
+console.log(nulo);
+console.log(indefinido);
+console.log(simbolo);
+console.log(numeroUno);
+
+// 5. Imprime por consola el tipo de todas las variables
+
+console.log(typeof comentarioUno);
+console.log(typeof comentarioDos);
+console.log(typeof edad);
+console.log(typeof esReal);
+console.log(typeof nulo);
+console.log(typeof indefinido);
+console.log(typeof simbolo);
+console.log(typeof numeroUno);
+
+// 6. A continuación, modifica los valores de las variables por otros del mismo tipo
+
+comentarioUno = "Nuevo comentario refresh 18:29";
+edad = 50;
+esReal = false;
+nulo = Object;
+indefinido = undefined;
+simbolo = Symbol("Nuevo simboloooo");
+numeroUno = BigInt(546545646545648468);
+
+// 7. A continuación, modifica los valores de las variables por otros de distinto tipo
+
+comentarioUno = 15;
+edad = "Edad en string";
+esReal = 45;
+nulo = undefined;
+indefinido = null;
+simbolo = 5645645n;
+numeroUno = Symbol("sadas");
+
+// 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
+
+
+// 9. A continuación, modifica los valores de las constantes
+
+
+// 10. Comenta las líneas que produzcan algún tipo de error al ejecutarse
+
