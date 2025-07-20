92be32286e6adbae1c562fb52c88cda711cee84e
Arielss <arielm.sereno.a@gmail.com>
2025-07-20 15:05:20 -0400

    Terminando proyectos

--- Curso básico JS/05 - Ejercicios operadores.js
+++ Curso básico JS/05 - Ejercicios operadores.js
@@ -1,15 +1,46 @@
 // 1. Crea una variable para cada operación aritmética
 
+let sumar = 5 + 2;
+let restar = 5 - 2;
+let multiplicacion = 5 * 2;
+let dividir = 5 / 2;
+
+let modulo = 5 % 2;
+let exponente = 5 ** 2;
+
 // 2. Crea una variable para cada tipo de operación de asignación,
 //    que haga uso de las variables utilizadas para las operaciones aritméticas
 
+sumar += 3;
+restar -= 1;
+multiplicacion *= 5;
+dividir /= 1;
+modulo %= 2;
+exponente **= 3;
+
 // 3. Imprime 5 comparaciones verdaderas con diferentes operadores de comparación
+
+console.log(5 > 3);
+console.log(2 < 8);
+console.log(5 == 5);
+console.log(5 >= 2);
+console.log(10 >= 10);
 
 // 4. Imprime 5 comparaciones falsas con diferentes operadores de comparación
 
+console.log(5 < 3);
+console.log(2 > 8);
+console.log(5 != 5);
+console.log(5 < 2);
+console.log(10 < 10);
+
 // 5. Utiliza el operador lógico and
 
+console.log(5 > 2 && 10 < 11);
+
 // 6. Utiliza el operador lógico or
+
+console.log(5 < 1 || 7 > 6);
 
 // 7. Combina ambos operadores lógicos
 
